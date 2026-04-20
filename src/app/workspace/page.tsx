8dcf3cc388d566fc63f3c7d06cc24384d68a087d
Nicolas Boitout <41285208+nboitout@users.noreply.github.com>
2026-04-20 23:43:54 +0300

    Change text from 'Teach' to 'Faculty'

--- src/app/workspace/page.tsx
+++ src/app/workspace/page.tsx
@@ -788,7 +788,7 @@ export default function WorkspacePage() {
                 <line x1="8.59" y1="13.51" x2="15.42" y2="17.49"/>
                 <line x1="15.41" y1="6.51" x2="8.59" y2="10.49"/>
               </svg>
-              Teach
+              Faculty
             </a>
             <button
               className={styles.signOutBtn}
